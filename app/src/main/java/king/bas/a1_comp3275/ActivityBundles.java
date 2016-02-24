aa80a742c9ba3f2a3deb2e5ca515cfd1179dba48
King Bas <Mr.Bascombe@live.com>
2016-02-24 01:07:20 -0400

    Completed Activity Bundle and Activty for result that includes the xmls and java

--- app/src/main/java/king/bas/a1_comp3275/ActivityBundles.java
+++ app/src/main/java/king/bas/a1_comp3275/ActivityBundles.java
@@ -1,11 +1,17 @@
 package king.bas.a1_comp3275;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityBundles extends AppCompatActivity {
 
@@ -16,7 +22,31 @@ public class ActivityBundles extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+    }// onCreate
 
-    }
+    public void logIn (View view){
+        Intent i = new Intent(ActivityBundles.this, ActivityResults.class);
+        Bundle bundle = new Bundle();
+        EditText eText = (EditText) findViewById(R.id.txt_name);
+        EditText pw = (EditText) findViewById(R.id.txt_pass);
+        String name = eText.getText().toString();
+        String passw = pw.getText().toString();
+
+        bundle.putString("name", name);
+        bundle.putString("password",passw);
+        i.putExtras(bundle);
+        startActivity(i);
+    }// logIn
+
+    public void Resulta (View view){
+        Bundle bundle2 = getIntent().getExtras();
+//        Get values from bundles using keys
+        String ans = bundle2.getString("results");
+
+        TextView result = (TextView)findViewById(R.id.txt_result);
+
+//        values for text view
+        result.setText(ans);
+    }// logIn
 
 }
--- app/src/main/java/king/bas/a1_comp3275/ActivityResults.java
+++ app/src/main/java/king/bas/a1_comp3275/ActivityResults.java
@@ -1,11 +1,14 @@
 package king.bas.a1_comp3275;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityResults extends AppCompatActivity {
 
@@ -16,6 +19,29 @@ public class ActivityResults extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
+        Bundle bundle = getIntent().getExtras();
+//        Get values from bundles using keys
+        String name = bundle.getString("name");
+        String password = bundle.getString("password");
 
-}
+        TextView nombre = (TextView)findViewById(R.id.txt_namer);
+        TextView passre = (TextView)findViewById(R.id.txt_passr);
+
+//        values for text view
+        nombre.setText(name);
+        passre.setText(password);
+
+    }// onCreate
+
+    public void Send (View view){
+        Intent i = new Intent(ActivityResults.this, ActivityBundles.class);
+        Bundle bundle2 = new Bundle();
+        EditText answer = (EditText) findViewById(R.id.txt_ok);
+        String ok = answer.getText().toString();
+
+        bundle2.putString("results", ok);
+        i.putExtras(bundle2);
+        startActivity(i);
+    }// Send
+
+}// Activity
