3ad2147b3ab4c6ea2333aa2d9428d2b53ac8efb3
King Bas <Mr.Bascombe@live.com>
2016-02-27 19:54:53 -0400

    Got the adapter to start by commenting out the image call

--- app/src/main/java/king/bas/a1_comp3275/ItemAdapter.java
+++ app/src/main/java/king/bas/a1_comp3275/ItemAdapter.java
@@ -1,53 +1,59 @@
 package king.bas.a1_comp3275;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by King on 27-Feb-16.
  */
 public class ItemAdapter extends ArrayAdapter<String> {
-    public final Context context;
     public ItemAdapter(Context context, String [] items){
         super (context, 0, items);
-        this.context = context;
     } //constructor
 
     @Override
     public View getView(int position, View v, ViewGroup p){
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         v = LayoutInflater.from(getContext()).inflate(R.layout.item, null);
         String item = getItem(position);
         String [] itemPrices = getContext().getResources().getStringArray(R.array.itemS_prices);
-        TypedArray itemImages = getContext().getResources().obtainTypedArray(R.array.items_images);
-            /*
-            final String [] itemDesc = getContext().getResources().getStringArray(R.array.items_description);
+//        TypedArray itemImages = getContext().getResources().obtainTypedArray(R.array.items_images);
 
-            ((Button) v.findViewById(R.id.btn_deets))
-                        .setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Toast.makeText(CustomAdapter.this, itemDesc[position], Toast.LENGTH_SHORT).show();
-                        }
-                    });
-*/
+//            final String [] itemDesc = getContext().getResources().getStringArray(R.array.items_description);
+//
+//            ((Button) v.findViewById(R.id.btn_deets))
+//                        .setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            Toast.makeText(ItemAdapter.this, itemDesc[position], Toast.LENGTH_SHORT).show();
+//                        }
+//                    });
+
         ((TextView)v.findViewById(R.id.txt_name))
                 .setText(item);
 
         ((TextView)v.findViewById(R.id.txt_price))
                 .setText(itemPrices[position]);
 
-        ((ImageView)v.findViewById(R.id.img_icon))
-                    .setImageResource(itemImages.getResourceId(position, 0));
+//        ((ImageView)v.findViewById(R.id.img_icon))
+//                    .setImageResource(itemImages.getResourceId(position, 0));
         return v;
 
     }// view
 
-}
+//    public void viewDeets(View view){
+//        final String [] itemDesc = getContext().getResources().getStringArray(R.array.items_description);
+//        Toast.makeText(ItemAdapter.this, itemDesc[position], Toast.LENGTH_SHORT).show();
+//    }// viewCart
+
+}// class
+
+
